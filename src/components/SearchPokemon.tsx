d0ab75f3bfcd05e1683b58ec7d61e69542a17c5a
Tob-private <tobias.riiser.games@gmail.com>
2025-08-26 10:43:49 +0200

    added search bar

--- src/components/SearchPokemon.tsx
+++ src/components/SearchPokemon.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
 export default function SearchPokemon() {
-    return (    
-        <article>
-            
+    return (
+        <article className="py-12 w-1/2 flex m-auto place-items-center">
+            <div className="relative w-full">
+                <Input type="text" placeholder="Search for a Pokémon..." className="py-5 pr-14" />
+                <Button type="submit" variant="outline" className="absolute right-2 top-1/2 -translate-y-1/2 bg-[#637CCE] p-3">
+                    <Image src={"/Search.svg"} width={15} height={15} alt="Search button" />
+                </Button>
+            </div>
         </article>
     );
 }
