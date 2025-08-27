fc7d9afed580b48006d15d75063cba55260ad723
Tob-private <tobias.riiser.games@gmail.com>
2025-08-27 09:53:35 +0200

    removed unused variable

--- src/app/search-result/[pokemon]/page.tsx
+++ src/app/search-result/[pokemon]/page.tsx
@@ -2,8 +2,6 @@ import PokemonCard from "@/components/PokemonCard";
 import { getPokemonByIDs } from "@/helpers/api";
 
 export default async function SearchResult({ params }: { params: { pokemon: string } }) {
-    const ids = Array.from({ length: 4 }, () => Math.ceil(Math.random() * 1000));
-
     const pokemon = await getPokemonByIDs([params.pokemon])
 
     return <PokemonCard pokemon={pokemon[0]} />;
