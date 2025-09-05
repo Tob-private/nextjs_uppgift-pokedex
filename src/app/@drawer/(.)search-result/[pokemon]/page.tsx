f4868010e64149568d0cd36fd34a4f40be3c3615
Tob-private <tobias.riiser.games@gmail.com>
2025-09-05 10:54:27 +0200

    corrected build errors

--- src/app/@drawer/(.)search-result/[pokemon]/page.tsx
+++ src/app/@drawer/(.)search-result/[pokemon]/page.tsx
@@ -1,7 +1,7 @@
 import DrawerWrapper from "@/components/search-pokemon/drawer";
 import { fetcher } from "@/helpers/api"
 
-export default async function ModalPage({ params }: { params: { pokemon: string } }) {
+export default async function ModalPage({ params }: { params: Promise<{ pokemon: string }> }) {
   const { pokemon } = await params
   const data = await fetcher(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
--- src/app/random-pokemon/[pokemon]/page.tsx
+++ src/app/random-pokemon/[pokemon]/page.tsx
@@ -1,7 +1,7 @@
 import ModalWrapper from "@/components/random-pokemon/modal-wrapper"
 import { fetcher } from "@/helpers/api"
 
-export default async function ModalPage({ params }: { params: { pokemon: string } }) {
+export default async function ModalPage({ params }: { params: Promise<{ pokemon: string }> }) {
   const { pokemon } = await params
   const data = await fetcher(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
